f9976fb761075ec664b66ef5c167965689019340
Naveen Natarajan <naveen.natarajan@cesltd.com>
2022-07-11 19:53:39 +0530

    Gorgias Sample uploaded

---
+++ GorgiasSample/GorgiasSample.Android/DeviceSpecificService.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GorgiasSample.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(DeviceSpecificService))]
+namespace GorgiasSample.Droid
+{
+    public class DeviceSpecificService : IDeviceSpecificService
+    {
+        public void ClearCookies()
+        {
+            Android.Webkit.CookieManager.Instance.RemoveAllCookie();
+        }
+    }
+}
---
+++ GorgiasSample/GorgiasSample/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+using Xamarin.Forms;
+
+namespace GorgiasSample.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            DependencyService.Get<IDeviceSpecificService>().ClearCookies();
+            webView.Reload();
+        }
+    }
+}
